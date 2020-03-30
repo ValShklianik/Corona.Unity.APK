2dec9f4b36905cc2dd29d17db8c9c960b1741ff5
Valeryia Shklianik <vshklianik@aras.com>
2020-03-30 16:09:42 +0300

    added corona png and corona moving on the screen

---
+++ CoronaWallpaper/Assets/Scripts/CoronaMoving.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoronaMoving : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCollidersAcrossScreen();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(2f, 2f), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Vector3 viewPos = transform.position;
+        //viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x*-1);
+        //viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y*-1);
+        //transform.position = viewPos;
+    }
+
+    void GenerateCollidersAcrossScreen()
+    {
+        Vector2 lDCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0f, Camera.main.nearClipPlane));
+        Vector2 rUCorner = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
+        Vector2[] colliderpoints;
+
+        EdgeCollider2D upperEdge = new GameObject("upperEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = upperEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, rUCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, rUCorner.y);
+        upperEdge.points = colliderpoints;
+
+        EdgeCollider2D lowerEdge = new GameObject("lowerEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = lowerEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+        lowerEdge.points = colliderpoints;
+
+        EdgeCollider2D leftEdge = new GameObject("leftEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = leftEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+        colliderpoints[1] = new Vector2(lDCorner.x, rUCorner.y);
+        leftEdge.points = colliderpoints;
+
+        EdgeCollider2D rightEdge = new GameObject("rightEdge").AddComponent<EdgeCollider2D>();
+
+        colliderpoints = rightEdge.points;
+        colliderpoints[0] = new Vector2(rUCorner.x, rUCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+        rightEdge.points = colliderpoints;
+    }
+}
---
+++ CoronaWallpaper/Assets/Scripts/MapRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRotation : MonoBehaviour
+{
+    private float a;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    a = 1;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    var rot = transform.rotation;
+    //    transform.Rotate(rot.x, rot.y + a, rot.z);
+    //}
+}
